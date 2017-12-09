6ce4d6aa4ea050172c36b0a9e31ee1298833757e
George Gore <georgelgore@gmail.com>
2017-12-09 17:40:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+  if (location > 42){
+    return location - 42
+  }
+  else {
+    return 42 - location
+  }
+}
+
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * 264
+}
+
+
+function distanceTravelledInFeet(start, end){
+  if (end > start) {
+    return (end - start) * 264;
+  }
+  else {
+    return (start - end) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    total = 0
+    switch (true) {
+      case distance <= 400:
+        total += 0;
+        break;
+      case distance < 2000:
+        total += (distance * .02)
+        break;
+      case distance > 2000:
+        if (distance < 2500){
+          total += 25
+          break;}
+        else {return 'cannot travel that far';}
+    }
+    return total
+}
